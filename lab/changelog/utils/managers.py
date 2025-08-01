7c29cc9a8ff49fc26189a166001e64ab4c0748e0
Federico Tabbò <federico.tabbo@networktocode.com>
2025-08-01 15:08:36 +0000

    Filter pre-query before sendig to DB

--- lab/changelog/utils/managers.py
+++ lab/changelog/utils/managers.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TypeVar
+from typing import Any, TypeVar
 
 from django.db import models
 
@@ -10,14 +10,14 @@ ModelT = TypeVar('ModelT', bound=models.Model, covariant=True)
 
 
 class FastInheritanceQuerySetMixin(InheritanceQuerySetMixin, models.QuerySet):
-    
+
     def select_subclasses(self, *subclasses: str | type[models.Model]) -> InheritanceQuerySet:
         if not subclasses:
             selected_subclasses = [x['target_model_name'] for x in self.values(
                 'target_model_name').distinct('target_model_name').order_by('target_model_name')]
         else:
             selected_subclasses = subclasses
-        
+
         try:
             return super().select_subclasses(*selected_subclasses)
         except ValueError:
@@ -33,7 +33,21 @@ class FastInheritanceManagerMixin(InheritanceManagerMixin):
 
 
 class FastInheritanceManager(FastInheritanceManagerMixin, InheritanceManager):
+    @classmethod
+    def _find_subclass(cls, obj):
+        return getattr(obj, obj.target_model_name)
 
-    def get_queryset(self) -> InheritanceQuerySet:
-        qs = super().get_queryset()
-        return qs.select_subclasses()
+    def all(self) -> InheritanceQuerySet:
+        return super().all().select_subclasses()
+
+    def filter(self, *args: Any, **kwargs: Any) -> InheritanceQuerySet:
+        return super().filter(*args, **kwargs).select_subclasses()
+
+    def get(self, *args: Any, **kwargs: Any) -> Any:
+        return self._find_subclass(super().get(*args, **kwargs))
+
+    def first(self, *args: Any, **kwargs: Any) -> Any:
+        return self._find_subclass(super().first(*args, **kwargs))
+
+    def last(self, *args: Any, **kwargs: Any) -> Any:
+        return self._find_subclass(super().last(*args, **kwargs))
